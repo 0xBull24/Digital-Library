04845886fdc85eb86b0c2514a82d4179baa9d709
D.Biles <darron.biles@gmail.com>
2019-01-13 23:58:57 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,29 @@
+// Dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+// Initialize the server
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+
+// Routes
+app.use(routes);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/digitallib');
+
+// Starting the server
+app.listen(PORT, () => {
+    console.log(`Server started on port`);
+    console.log(`Connect here http://localhost/${PORT}`);
+});
