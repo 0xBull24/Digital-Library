2c40a7e5224eb92bfa0ee39f1801cbbcdbf32a29
DBiles <Darron.biles@gmail.com>
2019-02-12 04:56:34 +0000

    wip: fixed search bar

--- client/src/pages/Search/search.js
+++ client/src/pages/Search/search.js
@@ -9,11 +9,17 @@ class Search extends Component {
 
     render() {
         return (
-            <div className='container-fluid'>
+            <div className='container-fluid center-align'>
                 <h4>Book Search</h4>
-                <div className='input-field col s12'>
-                    <input placeholder='Book Title' id='book-title' type='text' className='validate'></input>
-                    <button className='waves-effect waves-light btn right'>Search</button>
+                <div className="row center-align">
+                    <div className="col s12 center-align">
+                        <div className='input-field col s6 push-s3'>
+                            <input placeholder='Book Title' id='book-title' type='text' className='validate'></input>
+                        </div>
+                        <div className='input-field'>
+                            <button className='waves-effect waves-light btn right'>Search</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
